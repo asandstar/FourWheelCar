e431c33d576a91f4be078e7396d3f14aac2efad3
asandstar <89233112+asandstar@users.noreply.github.com>
2023-08-03 20:54:03 +0800

    之前完成了上云台的调试，现在完成所有云台的调试，上下均找到中立位及合适位置的脉宽

--- gimbal_two.ino
+++ gimbal_two.ino
@@ -8,13 +8,17 @@
 // #define SERVO_MIN_US 500   // 输入的PWM高电平脉宽最小为500us
 // #define SERVO_MAX_US 2500  // 输入的PWM高电平脉宽最大为2500us
 
-#define SERVO_MIN_US 544   // 输入的PWM高电平脉宽最小为500us
-#define SERVO_MAX_US 2400  // 输入的PWM高电平脉宽最大为2500us
-//min(可选)，舵机为最小角度（0度）时的脉冲宽度，单位为微秒，默认为544
-//max(可选)，舵机为最大角度（180度时）的脉冲宽度，单位为微秒，默认为2400
-#define SERVO_MID_US 1505  // 输入的PWM中立位脉宽最为1530us 每个人的车不太一样 微调
-// 以车在转弯半径中后轮的最靠近圆心一侧为基准测量转弯半径
+//gimbal_up
+#define SERVO_UP_MIN_US 930   //激光灯摆放较为合适的位置的下限脉宽，最下
+#define SERVO_UP_MAX_US 1330     //激光灯摆放较为合适的位置的上限脉宽，最上
+#define SERVO_UP_MID_US 1130  // 接近上臂竖直的脉宽，中立位
 
+//gimbal_down
+#define SERVO_DOWN_MIN_US 930   //下云台较为合适的位置的下限脉宽，最右（中立位右偏45度）
+#define SERVO_DOWN_MAX_US 1930     //下云台较为合适的位置的上限脉宽，最左（中立位左偏45度）
+#define SERVO_DOWN_MID_US 1430  // 中立位
+
+//16路i2c引脚和频率设定
 #define SERVO_FREQ 50    // 舵机PWM的频率是50Hz
 #define CHANNEL_GIMBAL_UP 15 //云台上层舵机脉冲 通道范围0~15
 #define CHANNEL_GIMBAL_DOWN 12 //云台下层舵机脉冲
@@ -31,7 +35,7 @@ void setServoPulse(uint8_t channel, double pulse_us) {
   pulselength /= 4096;               // 实际上一个周期固定是4096的长度 具体时长由setPWMFreq以及
                                      // setOscillatorFrequency决定 算出每个数对应的时长
   pulse_us /= pulselength;           // 算出正宽归一化到4096究竟占多少
-  pwm.setPWM(channel, 0, pulse_us);  // 利用Adafruit库的setPWM库设置channel从0~pulse_us
+  pwm.setPWM(channel, 0, pulse_us);  //  利用Adafruit库的setPWM库设置channel从0~pulse_us
                                      // 高电平 再然后全是低电平
 }
 
@@ -49,30 +53,84 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //云台双舵机测试
-   setServoPulse(CHANNEL_GIMBAL_UP, 102);
-   delay(1000);
-  // setServoPulse(CHANNEL_GIMBAL_UP, 187);
-  // delay(1000);
-   setServoPulse(CHANNEL_GIMBAL_UP, 280);
-   delay(1000);  
-  // setServoPulse(CHANNEL_GIMBAL_UP, 373);
-  // delay(1000);
-  setServoPulse(CHANNEL_GIMBAL_UP, 510);
+//中立位
+  setServoPulse(CHANNEL_GIMBAL_UP, SERVO_UP_MID_US);
+  setServoPulse(CHANNEL_GIMBAL_DOWN, SERVO_DOWN_MID_US);
   delay(1000);
-  setServoPulse(CHANNEL_GIMBAL_UP, 900);
-  delay(1000);
-  // setServoPulse(CHANNEL_GIMBAL_UP, 1000);
-  // delay(1000);
-  // setServoPulse(CHANNEL_GIMBAL_UP, 1100);
-  // delay(1000);
-  setServoPulse(CHANNEL_GIMBAL_UP, 1140);
-  delay(1000);
-  // setServoPulse(CHANNEL_GIMBAL_UP, 1200);
-  // delay(1000);
-  // setServoPulse(CHANNEL_GIMBAL_UP, 1300);
-  // delay(1000);
-  // setServoPulse(CHANNEL_GIMBAL_UP, 1400);
-  // delay(1000);
+  //先断舵机的电，再断连arduino防止断电后脱离中立位
 
 }
+
+
+
+
+  //云台双舵机测试
+  // //适合放激光笔的几个角度之一
+  // setServoPulse(CHANNEL_GIMBAL_UP, 930);
+  // delay(1000);
+  // //中立位
+  // setServoPulse(CHANNEL_GIMBAL_UP, 1130);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_UP, 1330);
+  // delay(1000);
+  // // setServoPulse(CHANNEL_GIMBAL_UP, 1330);
+  // // delay(1000);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_UP, 1130);
+  // delay(1000);
+
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 130);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 330);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 530);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 730);
+  // delay(1000);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 930);
+  // delay(3000); 
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 1430);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 1930);
+  // delay(3000);
+  // delay(3000);
+  //以上云台有线处为反方向，下云台逆时针数值变大，也就是左边数值大于右边
+  //低头数值变小，抬头数值变大
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 530);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 330);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 130);
+  // delay(1000);
+  // delay(1000);
+  // delay(1000);
+
+  //start
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 930);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 1130);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 1330);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 1530);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 1730);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 1930);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 1530);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 1130);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 730);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 330);
+  // delay(1000);
+  // //后面的不转了
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 2130);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 2330);
+  // delay(1000);
+  // setServoPulse(CHANNEL_GIMBAL_DOWN, 2530);
+  // delay(1000);
